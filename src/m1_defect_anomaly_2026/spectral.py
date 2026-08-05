715b4bf5e4d177a4f3fd42749f4d5df54fa58b2d
mary-ammar-univ <mary.ammar@etu.unistra.fr>
2026-08-05 13:28:20 +0000

    six features

--- src/m1_defect_anomaly_2026/spectral.py
+++ src/m1_defect_anomaly_2026/spectral.py
@@ -18,6 +18,34 @@ def power_spectrum(beat,fs=FS,use_window=True):
 
 
 
+# gives six numbers describing one beat's spectrum
+def spectral_features(beat,fs=FS):
+    freqs,power=power_spectrum(beat,fs)
+    total=power.sum()+1e-12
+    p=power/total # we use it to normalise and behaves like a probab distribution
+
+    centroid=float((freqs*p).sum())
+    bandwidth= float(np.sqrt((((freqs-centroid)**2)*p).sum()))
+    entropy=float(-(p*np.log(p+1e-12)).sum()/np.log(len(p)))
+    dominant=float(freqs[np.argmax(power)])
+    lf_ratio=float(p[freqs<10].sum())
+    hf_ratio=float(p[freqs>20].sum())
+
+    return {"spectral_centroid":centroid,
+            "spectral_bandwdth":bandwidth,
+            "spectral_entropy":entropy,
+            "dominant_freq":dominant,
+            "lf_ratio":lf_ratio,
+            "hf_ratio":hf_ratio}
+
+
+# apply spectral_features to every beat
+def beat_feature_matrix(beat_matrix,fs=FS):
+    return pd.DataFrame([spectral_features(b,fs) for b in beat_matrix])
+
+
+
+
 
 
 
